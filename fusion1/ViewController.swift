09a53905c9d027813cdb46fbef0d014ea9dfbb20
Rakesh Soni <rsoni@paypal.com>
2015-12-25 16:38:21 -0800

    Initial Commit

---
+++ fusion1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fusion1
+//
+//  Created by Soni, Rakesh on 12/25/15.
+//  Copyright © 2015 Soni, Rakesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
